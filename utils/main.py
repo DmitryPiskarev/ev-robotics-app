2d37c434504508291f946f7d1d4ae095d59596ab
Dmitry Piskarev <piskarev.dmitry25@gmail.com>
2025-09-04 16:36:58 +0300

    utils change

--- utils/main.py
+++ utils/main.py
@@ -1,5 +1,6 @@
 from .geometry import get_coords_from_y, get_coords_x_frame, circle_intersections
 import numpy as np
+from scipy.optimize import minimize
 
 
 def suspension_positions(phi_deg, l_lca, l_uca, inner_dist, ang_deg, outer_dist):
@@ -40,3 +41,46 @@ def wheel_travel(phi_deg_range, l_lca, l_uca, inner_dist, ang_deg, outer_dist):
         _, _, _, UCA_out = pos
         travels.append(UCA_out[1])
     return phi_deg_range[:len(travels)], travels
+
+
+def optimize_suspension(l_lca, l_uca, outer_dist, inner_dist,
+                        inner_fixed=None, inner_y_fixed=None, tie_fixed=None,
+                        ang_fixed=None, offset_fixed=None):
+    def objective(vars):
+        inner_x, inner_y, t_pickup, ang_deg, offset_dist = vars
+        inner_x_val = inner_x if inner_fixed is None else inner_fixed
+        inner_y_val = inner_y if inner_y_fixed is None else inner_y_fixed
+        t_val = t_pickup if tie_fixed is None else tie_fixed
+        ang_val = ang_deg if ang_fixed is None else ang_fixed
+        offset_val = offset_dist if offset_fixed is None else offset_fixed
+        _, deviations = tie_rod_deviation(
+            inner_xy=(inner_x_val, inner_y_val),
+            t_on_knuckle=t_val,
+            l_lca=l_lca,
+            l_uca=l_uca,
+            inner_dist=inner_dist,
+            ang_deg=ang_val,
+            outer_dist=outer_dist,
+            offset_dist=offset_val
+        )
+        return np.max(np.abs(deviations))
+
+    x0 = [
+        0.0 if inner_fixed is None else inner_fixed,
+        19.3 if inner_y_fixed is None else inner_y_fixed,
+        0.36 if tie_fixed is None else tie_fixed,
+        -9.8 if ang_fixed is None else ang_fixed,
+        -2.6 if offset_fixed is None else offset_fixed
+    ]
+
+    bounds = [
+        (-50, 50) if inner_fixed is None else (inner_fixed, inner_fixed),
+        (0, 80) if inner_y_fixed is None else (inner_y_fixed, inner_y_fixed),
+        (0, 1) if tie_fixed is None else (tie_fixed, tie_fixed),
+        (-20, 20) if ang_fixed is None else (ang_fixed, ang_fixed),
+        (-20, 20) if offset_fixed is None else (offset_fixed, offset_fixed)
+    ]
+
+    res = minimize(objective, x0, bounds=bounds, method='L-BFGS-B')
+    return res.x
+
